6703cb84f3083e9d928abce77638346d48b91ec1
Marcus Kazmierczak <marcus@mkaz.com>
2015-10-29 20:41:28 -0700

    Fix media upload, parameter name requires media[]. Switch to use anonymous struct and check a value gets returned to squelch a index not found error

--- media.go
+++ media.go
@@ -6,18 +6,17 @@ import (
 	"log"
 )
 
-type UploadResult struct {
-	Media []struct {
-		Link  string
-		Title string
-	}
-}
-
+// upload a single file
 func upload_media(filename string) {
-	var ur UploadResult
+	var ur struct {
+		Media []struct {
+			Link  string
+			Title string
+		}
+	}
 
 	j := getApiFetcher("media/new")
-	j.Files["media"] = filename
+	j.Files["media[]"] = filename
 	resp, err := j.Method("POST").Send()
 
 	if err != nil {
@@ -28,5 +27,11 @@ func upload_media(filename string) {
 		log.Fatal("Error parsing:", err)
 	}
 
-	fmt.Println(ur.Media[0].Link)
+	if len(ur.Media) > 0 {
+		fmt.Println(ur.Media[0].Link)
+	} else {
+		fmt.Println("Error: No link in results")
+		fmt.Println(resp.StatusCode)
+	}
+
 }
